6deeccaca95f68518c5956fbea3b56864750cc07
gerogiordano08 <85655339+gerogiordano08@users.noreply.github.com>
2025-04-15 13:09:10 -0300

    Creé los ultimos tests para casos extremos

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -7,14 +7,23 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("reconocer"))
         self.assertFalse(is_palindrome("mamadera"))
         self.assertFalse(is_palindrome("computadora"))
+    
     def test_frases_palindromes(self):
         self.assertTrue(is_palindrome("anita lava la tina"))
         self.assertTrue(is_palindrome("a santa at nasa"))
         self.assertTrue(is_palindrome("amo la pacifica paloma"))
+    
     def test_frases_no_palindromes(self):
         self.assertFalse(is_palindrome("hola, como estas?"))
         self.assertFalse(is_palindrome("hoy hace frio"))
         self.assertFalse(is_palindrome("amo la buena paloma"))
+    
+    def test_casos_edge(self):
+        self.assertTrue(is_palindrome(""))
+        self.assertTrue(is_palindrome("a"))
+        self.assertTrue(is_palindrome("ReCoNoCeR"))
+        self.assertTrue(is_palindrome("No 'x' in Nixon"))
+        self.assertTrue(is_palindrome("12321"))
 
 if __name__ == '__main__':
     unittest.main()
